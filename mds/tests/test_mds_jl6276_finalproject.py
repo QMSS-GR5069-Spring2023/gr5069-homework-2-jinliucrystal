1b2e9901505d564548871a20e994820b33e8eaab
Krystal Hahn <kmh2259@columbia.edu>
2023-03-09 05:28:04 -0500

    Add global definitions section to test script

--- mds/tests/test_mds_jl6276_finalproject.py
+++ mds/tests/test_mds_jl6276_finalproject.py
@@ -1,3 +1,6 @@
+# ::::::: GLOBAL DEFINITIONS :::::::::::::::::::::
+
+# Load packages and modules
 from mds_jl6276_finalproject import mds_jl6276_finalproject
 import folium
 from folium.plugins import MarkerCluster
