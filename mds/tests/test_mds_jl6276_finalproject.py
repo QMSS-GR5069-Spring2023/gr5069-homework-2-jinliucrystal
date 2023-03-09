969cb2c087887cb730c253239b59d92b53e56157
jin <jl6276@columbia.edu>
2023-03-09 17:02:21 -0500

    resolved

--- mds/tests/test_mds_jl6276_finalproject.py
+++ mds/tests/test_mds_jl6276_finalproject.py
@@ -1,9 +1,15 @@
-# ::::::: GLOBAL DEFINITIONS :::::::::::::::::::::
+ ::::::: GLOBAL DEFINITIONS :::::::::::::::::::::
 
-# Load packages and modules
-from mds_jl6276_finalproject import mds_jl6276_finalproject
-import folium
-from folium.plugins import MarkerCluster
+ # Load packages and modules
+ from mds_jl6276_finalproject import mds_jl6276_finalproject
+ import folium
+ from folium.plugins import MarkerCluster
+ def test():
+     m = folium.Map(location=[40.7128, -74.0060], zoom_start=11)
+     expected = m
+     actual = mds_jl6276_finalproject.get_map('spanish')
+     assert type(actual) == type(expected), "Result should be an map object"
+     return actual
 
 
 
